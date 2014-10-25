fb25666826bdf595a02c7ecd071f56ff11569357
prav33n <praveenjelish@ymail.com>
2014-10-25 13:05:08 +0200

    added view extracted mails to the application

--- showExtractedMails.php
+++ showExtractedMails.php
@@ -1,3 +1,8 @@
+<?php
+  require("OutLook.php");
+  require("db.php");
+?>
+
 <html lang="en">
   <head>
     <title>Outlook Mail view</title>
@@ -7,9 +12,32 @@
     <link href="css/outlook.css" rel="stylesheet">
   </head>
   <body>
-    <a href="index.php">Back</a>
+    <a href="index.php"><h4>&larr; Back<h4><br></a>
     <?php
-    echo "Mail contents";
+    $db = new db;
+    $sql = "SELECT * FROM email ORDER BY folderid";
+    $result = $db->db_query($sql);
+    echo '<div class="formContainer">
+            <table class="table table-striped table-hover">
+              <tbody>';
+    echo '<thead>
+          <tr>
+          <th>Sender Name</th>
+          <th>CC/BCC</th>
+          <th>Subject</th>
+          <th>Folder</th>
+          </tr>
+          </thead>';
+    while($resutlArray = $result->fetch_object()){
+        echo '<tr>';
+        $row = $resutlArray->senderName !== '' ? $resutlArray->senderName : $resutlArray->senderEmail;
+        echo '<td id = "name">'.$row.'</td>';
+        echo '<td id = "cc">'.$resutlArray->cc.'<br>'.$resutlArray->bcc.'</td>'; 
+        echo '<td id = "subject">'.$resutlArray->subject.'</td>';
+        echo '<td id = "folderName">'.$resutlArray->foldername.'</td>';
+        echo '</tr>';
+    }
+    echo '</tbody></table></div>';
     ?>
   </body>
 </html>
